f9380ffddadb3a6633624d084a562f4eda07b47b
GlendyT <glendytuyuc@gmail.com>
2024-06-14 16:41:30 -0600

    obtener proyectos desde react, evitar repetir enviar token con interceptors, crear un endpoint y hook con la info del usuario, definiendo shcema del usuario , mostrar info del usuario en el menu de hamburguesa, cerrando sesion y redireccionar al iniciar sesion

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,19 @@
 import axios from "axios";
 
+
 const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL
 })
 
+api.interceptors.request.use( config => {
+    const token = localStorage.getItem("AUTH_TOKEN")
+    if(token) {
+        config.headers.Authorization = ` Bearer ${token}`
+    }
+    return config
+})
+
 export default api
+
+//TODO: REQUEST ANTES DE LA PETICION HTTP Y ENVIA EL TOKEN
+//TODO: RESPONSE SE EJECUTA DESPUES DE LA PETICION
